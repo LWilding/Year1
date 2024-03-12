be5338db9830212bcc53240ec5c3de172e8a6872
Wilding <L.Wilding2539@student.leedsbeckett.ac.uk>
2024-03-12 15:55:13 +0000

    week 2

--- practicals/03-control-statements/Programs 02.py
+++ practicals/03-control-statements/Programs 02.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 
+name = input("Hello, what is your your name? ")
+if name:
+    print(f"Hello, {name}. Good to meet you!")
+
+
+
+celsius = int(input("Enter a temperature in Celsius: "))
+if celsius:
+    print(f"{celsius} C is equivalent to", (celsius*9/5)+32,"F")
+
+
+
+
+total_students = int(input("How many students?"))
+group_size = int(input("Required group size?"))
+
--- practicals/03-control-statements/Test.py
+++ practicals/03-control-statements/Test.py
@@ -1,3 +1,6 @@
 
 
-if name == "Luke":
+num = int(input("please enter a number between 1 and 10"))
+if num >=1 and num <=10:
+    print("In the range")
+
