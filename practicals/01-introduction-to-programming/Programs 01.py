b16747f555129d2c59a61a51d985c1af57d35314
Luke Wilding <lukewilding@Lukes-iMac.local>
2024-03-16 14:17:52 +0000

    Programs add

--- practicals/01-introduction-to-programming/Programs 01.py
+++ practicals/01-introduction-to-programming/Programs 01.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-
-if __name__ == '__main__' :
+# 1
+if __name__ == '__main__':
     print('Hello, World')
     print('Hello')
 
+# 2
+print('Hello, Luke!')
 
 
+# 3
+C = 38.4
+F = round(C * 9 / 5 + 32, 1)
+print(f'The Temperature is {C}C and {F}F')
 
 
+# 4
+out = 1014 - 162
+b_avg = round(48426 / out, 2)
 
+print(f"Boycott's batting average is {b_avg}")
--- practicals/03-control-statements/Programs 02.py
+++ practicals/03-control-statements/Programs 02.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-name = input("Hello, what is your your name? ")
+name = input('Hello, what is your your name? ')
 if name:
-    print(f"Hello, {name}. Good to meet you!")
+    print(f'Hello, {name}. Good to meet you!')
 
 
 
-celsius = int(input("Enter a temperature in Celsius: "))
+celsius = int(input('Enter a temperature in Celsius: '))
 if celsius:
-    print(f"{celsius} C is equivalent to", (celsius*9/5)+32,"F")
+    print(f'{celsius}C is equivalent to {celsius*9/5+32}F')
 
 
 
 
-total_students = int(input("How many students?"))
-group_size = int(input("Required group size?"))
+total_students = int(input('How many students? '))
+group_size = int(input('Required group size? '))
 
