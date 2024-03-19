2afccab15bf0446c78fd11ca0c52f89342f77e57
Wilding <L.Wilding2539@student.leedsbeckett.ac.uk>
2024-03-19 15:53:09 +0000

    Exercises 04

---
+++ practicals/04-defining-functions/Exercises 04.py
@@ -0,0 +1,20 @@
+
+
+def print_header(msg):
+    print('*****', msg)
+
+print_header('hello')
+
+
+
+
+def find_min(a,b):
+    if a < b:
+        min = a
+    else:
+        min = b
+    return min
+
+print(find_min(10,5))
+
+
