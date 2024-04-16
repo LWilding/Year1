f1210cf36730fa360437afd3f71a3578d18abe81
Wilding <L.Wilding2539@student.leedsbeckett.ac.uk>
2024-04-16 15:53:43 +0100

    programs week 3

---
+++ practicals/03-control-statements/Programs 03 Task 2.py
@@ -0,0 +1,8 @@
+
+password = input('Please enter a password: ')
+repeat = input('Repeat password: ')
+if password == repeat:
+    print('Password Set')
+else:
+    print('Error password did not match')
+
---
+++ practicals/03-control-statements/Programs 03 Task 3.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+
+    while True:
+        password = input('Please enter a password(8-12 characters long): ')
+        repeat = input('Please re enter your password: ')
+        if 7 <len(password) < 13 and password == repeat:
+            print('Password Set')
+            break
+        if password != repeat:
+            print('Error password did not match')
+        else:
+            print('Error incorrect length')
